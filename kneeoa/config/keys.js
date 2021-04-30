f10f1baac4457d98ecb78c7e5d7d191fa9dd434a
Tommy NgX <58539340+Tommy-Ngx@users.noreply.github.com>
2021-04-30 12:01:09 +1000

    Add files via upload

---
+++ kneeoa/config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://admin-thaman:'+ encodeURIComponent('admin-thaman') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: dbPassword
+};
